410269ca4efb5b62819430ac346234d24be1a2e1
kingsleyocran <ocrankingsley@gmail.com>
2022-11-01 19:11:29 +0000

    add task 3

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A function that requests all of the hot_list articles of a subreddit"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """Function that requests all of the hot_list articles of a subreddit"""
+
+    URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    params = {'limit': 100}
+    headers = {}
+    if after:
+        params['after'] = after
+
+    headers = {'User-Agent': ''}
+    r = requests.get(URL, headers=headers, params=params)
+
+    if (r.status_code != 200 and after == None):
+        return (None)
+    r = r.json()
+    posts = r['data']['children']
+    for post in posts:
+        hot_list.append(post['data']['title'])
+    if r['data']['after']:
+        return (recurse(subreddit, hot_list, after=r['data']['after']))
+    else:
+        return (hot_list)
